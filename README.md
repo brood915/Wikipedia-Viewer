001c048626c4129733981fe81c4afcb1fe8972cd
k <brood915@gmail.com>
2017-04-14 10:17:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ReactJS Boilerplate with webpack 2
+
+```
+ES6
+React
+SASS/SCSS
+html-webpack-plugins
+react-router
+fetch-jsonp
+```
+
+
+### To run this application
+
+```
+npm install to install all dependencies
+npm run start to start webpack-dev-server
+server running at localhost:1337
+npm run build for production
+```
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: ['./index.js', './style/main.scss'],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, 
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+      use: 'babel-loader'
+      }, //babel
+      { // regular css files
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?importLoaders=1',
+        }),
+      },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  },
+  devServer: {
+  contentBase: path.join(__dirname, "dist"),
+  compress: true,
+  port: 1337
+},
+  plugins: [
+    new HtmlWebpackPlugin({template: './index.html'}),
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'bundle.css',
+      allChunks: true,
+    }),
+  ]
+};
